dd67ec419cb7fe315d8b0b69d5de5db16752f608
AlejandroBast <nicohlas.personal@gmail.com>
2026-07-04 18:34:57 -0500

    eliminacion de row comuna

--- scraper_ciencuadras.py
+++ scraper_ciencuadras.py
@@ -25,9 +25,9 @@ BASE_URL = "https://www.ciencuadras.com"
 
 DB_CONFIG = {
     "host": os.getenv("DB_HOST", "localhost"),
-    "port": int(os.getenv("DB_PORT", "3306")),
+    "port": int(os.getenv("DB_PORT", "3301")),
     "user": os.getenv("DB_USER", "root"),
-    "password": os.getenv("DB_PASSWORD", "boludo123"),
+    "password": os.getenv("DB_PASSWORD", "Nico123"),
     "database": os.getenv("DB_NAME", "db_inmobiliary_data"),
 }
 
